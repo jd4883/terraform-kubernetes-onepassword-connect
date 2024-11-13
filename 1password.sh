e95c919b705b66a8e278b07197cf912d1c80d2bb
Condoriano <15719920+jd4883@users.noreply.github.com>
2024-11-13 03:12:02 +0000

    Update 1password.sh

--- 1password.sh
+++ 1password.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-BASE=$(kubectl get secrets -n onepassword onepassword-token -o json | jq '.data | map_values(@base64d)'|jq .token)
+BASE=$(kubectl get secrets -n cluster-tools onepassword-token -o json | jq '.data | map_values(@base64d)'|jq .token)
 OUTPUT="{ \"token\": $BASE }"
 echo $OUTPUT|jq
